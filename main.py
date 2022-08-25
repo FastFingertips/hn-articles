ef7ea48bb7a059d12d8fabb237bad65f10bdb257
FastFingertips <bugra.coskun@outlook.com>
2022-08-25 21:30:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import time as t
-from SwSpotify import spotify
-import pyautogui as pg
-import json
 import requests
 import webbrowser
+from datetime import datetime
+import json
 
+def getTimestamp():
+    dt = datetime.now()
+    timestamp = int(datetime.timestamp(dt))
+    return timestamp
+
+def timestampConverter(timestamp):
+    dt = datetime.fromtimestamp(timestamp)
+    sdt = dt.strftime("%Y.%m.%d, %H:%M:%S")
+    return sdt
+
+def readtxtfilelines():
+    with open("urls.txt", 'r') as f:
+        return f.readlines()
+
+def urlcheck(url):
+    import os
+    filename = "urls.json"
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            json.dump({}, f)
+            print("File created")
+            t.sleep(4)
+
+    jsonFile = open(filename, "r") # Open the JSON file for reading
+    data = json.load(jsonFile) # Read the JSON into the buffer
+    jsonFile.close() # Close the JSON file
+
+    print('Visit: ', end='')
+    if url in data.keys(): 
+        print(timestampConverter(int(data[url])))
+        return False
+    else:
+        data[url] = str(getTimestamp())
+        jsonFile = open(filename, "w+") # Save our changes to JSON file
+        jsonFile.write(json.dumps(data, indent=4))
+        jsonFile.close()
+        print(timestampConverter(getTimestamp()), 'New Visit!')
+        return True
 
 def getArticles():
     url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -44,56 +81,57 @@ def getArticleText(id): # returns the text of the article
     article = getArticle(id)
     return article['text']
 
+def articleParser(article_json):
+    g =     {'id':None,
+            'by':None,
+            'descendants': 0, # sometimes article has no comments
+            'kids':[],
+            'score':None,
+            'time':None,
+            'title':None,
+            'type':None,
+            'url':''}
+    r =    {}
+    for getKey , exceptValue in g.items():
+        try: r[getKey] = article_json[getKey]
+        except KeyError:
+            if exceptValue != None: r[getKey] = exceptValue
+            else: print(f'KeyError: {getKey}')
+    return r
+
+def dictToTable(dict, table_name):
+    from rich.console import Console
+    from rich.table import Table
+    console = Console()
+    t = Table(title=table_name)
+    for col in dict.keys(): t.add_column(col)
+    rows = []
+    for row in dict.values(): 
+        try: rows.append(str(row))
+        except: rows.append(row)
+    t.add_row(*rows)
+    console.print(t)
+
 def main():
     articleIds = getArticles()
     print(len(articleIds))
-    no = 1
-    for articleId in articleIds:
-        article = getArticle(articleId)
-        author = article['by']
-        try: comments = article['descendants']
-        except KeyError: comments = 0
-        id = article['id']
-        try: kids = article['kids']
-        except KeyError: kids = []
-        score = article['score']
-        time = article['time']
-        title = article['title']
-        type = article['type']
-        try: 
-            url = article['url'] 
-        except KeyError: url = ''
-        articleDict = {'id': id,
-                       'author': author,
-                       'comments': comments,
-                       'kids': kids,
-                       'score': score,
-                       'time': time,
-                       'title': title,
-                       'type': type,
-                       'url': url}
-        print(f'{no}: {type}')
-        # printing all the article info
-        maxLenghtKey = max(len(key) for key in articleDict)
+    for rank, articleId in enumerate(articleIds):
+        articleJson = getArticle(articleId)
+        articleDict = articleParser(articleJson)
+
+        print(f'{rank+1}: {articleDict["type"]}')
+        # dictToTable(articleDict, articleDict["title"])
         for key, value in articleDict.items():
-                print(f'{key}:{" "*(maxLenghtKey-len(key))} {value}')
+            maxLenghtKey = max(len(key) for key in articleDict)
+            print(f'{key}:{" "*(maxLenghtKey-len(key))} {value}')
+
+        url = articleDict['url']
         if url != '':
-            with open('urls.txt', "r+") as f:
-                urls = f.readlines()
-                if  url+'\n' not in urls:
-                    with open('urls.txt', "a+") as f:
-                        f.write(f"{url}\n")
-                    webbrowser.open(url, new=0, autoraise=True)
-                    t.sleep(20)
-                else:
-                    print("Already read")
-        no += 1
+            filechange = urlcheck(url)
+            if filechange:
+                webbrowser.open(url, new=2)
+                t.sleep(0)
         print('\n')
 
 if __name__ == "__main__":
     main()
-    import webbrowser
-
-url = 'https://pythonexamples.org'
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
-webbrowser.get('chrome').open(url)
